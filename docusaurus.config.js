badb088e3dc4cf10f52a0e2384b4a3224eaace4c
Byeong Min Cho <merrybmc@gmail.com>
2023-02-07 19:57:45 +0900

    test2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,9 +10,9 @@ const config = {
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
   url: 'https://merrybmc.github.io',
-  baseUrl: '/TIL/',
+  baseUrl: '/',
   organizationName: 'merrybmc',
-  projectName: 'TIL',
+  projectName: 'merrybmc.github.io',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   i18n: {
